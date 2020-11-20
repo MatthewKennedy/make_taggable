4b70a06426950ee39c7a19b48cabe3172b98eeac
Matthew Kennedy <m.kennedy@me.com>
2020-11-20 00:47:23 +0000

    Test for rails 5

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -2,16 +2,20 @@ require_relative 'boot'
 
 require "rails"
 # Pick the frameworks you want:
+unless Rails.gem_version < Gem::Version.new("6.0")
+  require "action_mailer/railtie"
+  require "action_mailbox/engine"
+  require "action_text/engine"
+end
+
+require "action_view/railtie"
+require "action_cable/engine"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
+
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
