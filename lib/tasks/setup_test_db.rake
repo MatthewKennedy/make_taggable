d7e5a8c92eb0722b993e6ace6da284f8658ca6f7
Matthew Kennedy <m.kennedy@me.com>
2020-12-14 23:02:18 +0000

    try removing

--- lib/tasks/setup_test_db.rake
+++ lib/tasks/setup_test_db.rake
@@ -1,5 +1,6 @@
-desc "Migrate db"
 
+desc "Migrate db"
 task :set_db do
   exec "cd spec/dummy && rake make_taggable_engine:install:migrations && rake db:create && rake db:migrate"
 end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,10 +7,3 @@ require "rspec/rails"
 require "rails"
 
 Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
-
-RSpec.configure do |config|
-  config.fixture_path = "spec/fixtures"
-  config.global_fixtures = :all
-  config.use_transactional_fixtures = true
-  config.infer_base_class_for_anonymous_controllers = true
-end
