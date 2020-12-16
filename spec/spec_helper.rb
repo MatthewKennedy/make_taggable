ac360786b7de3fcecac5a14b787fd5fc5f72a026
Matthew Kennedy <m.kennedy@me.com>
2020-12-16 15:13:02 +0000

    remove requirement.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] = "test"
 
 require_relative "../spec/dummy/config/environment"
 
-require "rspec/rails"
 require "rails"
 
 Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
